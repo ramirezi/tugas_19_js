93f2920a721f748506385be39fcbf326ffbeb86b
Ferry <ferryhermawan864@gmail.com>
2019-11-28 21:46:44 +0700

    Tugas 19 JS

---
+++ tugas19.js
@@ -0,0 +1,16 @@
+function pengulangan(){
+var kata = "Semangat...";
+console.log(kata.repeat(5));
+
+var bintang = "*****";
+console.log(bintang);
+console.log(bintang.repeat(5));
+console.log(bintang.repeat(10));
+console.log(bintang.repeat(15));
+console.log(bintang.repeat(20));
+console.log(bintang.repeat(15));
+console.log(bintang.repeat(10));
+console.log(bintang.repeat(5));
+console.log(bintang);
+}
+pengulangan();
